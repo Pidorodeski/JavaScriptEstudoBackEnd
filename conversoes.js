c2eacaa997a73e65c80413cfa5e944f6c56a1bc0
Cristian <cristian.00.bill@gmail.com>
2022-04-12 19:40:05 -0300

    refs#00000 - Adicionado arquivo de conversões

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// Tipo de dado
+// Booleanos
+
+// Conversão Implicita
+const numero = 456;
+const numeroString = "456";
+
+
+
+console.log(numero == numeroString);
+
+// Conversão explícita
+const numeroStringDois = Number("456");
+console.log(numero + Number(numeroString));
+
+console.log(numero + numeroStringDois)
